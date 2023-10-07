17c58a1005bb6feb49a4ff51750c5e4ce74e6734
Aaditya Jain <133569212+Aaditya019Jain@users.noreply.github.com>
2023-10-07 16:06:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,19 @@ export default function App() {
     <div className="App">
       <MenteeForm />
     </div>
+          <Navbar title="SOC"/>
+          <Textform />
+          <div class="containerscrollable">
+            <Reviews Name="abc" text= "what he has to say" value={0}/>
+            <Reviews Name="abc" text= "what he has to say" value={50}/>
+            <Reviews Name="cde" text= "what he has to say" value={50}/>
+            <div>
+              <button onClick={<Button/>}>
+                click
+              </button>
+            </div>
+          </div>
+      </div>
   </>
   );
 }
