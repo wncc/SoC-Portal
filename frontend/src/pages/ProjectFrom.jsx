492e0215bec5d961f3635443a07d1a8d2d9672e9
Aaditya Jain <133569212+Aaditya019Jain@users.noreply.github.com>
2023-10-15 00:01:11 +0530

    Updated project form

--- frontend/src/pages/ProjectFrom.jsx
+++ frontend/src/pages/ProjectFrom.jsx
@@ -9,15 +9,36 @@ import MyComponent2 from "../components/MyComponent2";
 export default function ProjectForm(){
 
     const [projectlist , setProjectlist] = useState({
-        title : '',
-        cateogary : '',
-        mentee_min : '',
-        mentee_max : '',
-        description : '',
-        timeline : '',
-        banner_image : '',
+        title: '',
+        abc: '',
+        categary: '',
+        mentee_min: '',
+        mentee_max: '',
+        description: '',
+        timeline: '',
+        banner_image: '',
 
     })
+
+    const detailsChange = (e) => {
+        const {id,value} = e.target
+        setProjectlist({
+          ...projectlist,
+            [id] : value,
+            // console.log(projectlist)
+        })
+        console.log('id:', id);
+        console.log('value:', value);
+    }
+
+    const detailsSubmit = (e) => {
+        console.log(projectlist)
+        axios.post('/api/dashboard/mentor/submit/',projectlist)
+        .then(res =>{
+            console.log(res)
+        }).catch(err =>
+             console.log(err))
+    }
 
 
     const handleRollNumberChange = (e) => {
@@ -29,6 +50,7 @@ export default function ProjectForm(){
             if(roll_number.data.length === 0){
                 alert("No such user exists")
                 e.target.style.backgroundColor = 'red';
+
 
             }
             else{
@@ -51,7 +73,7 @@ export default function ProjectForm(){
             <h1>Project Form</h1>
             <form>
                 <label>Project Title</label>
-                <input type='text' id='title'/>
+                <input type='text' id='title' onChange={detailsChange}/>
                 <br></br>
                 <label>
                     Co Mentors 
@@ -63,54 +85,54 @@ export default function ProjectForm(){
                 <br></br>
                 <label>
                     Title your project idea   
-                    <input  type='text' requried/>
+                    <input  type='text' id='abc' requried onChange={detailsChange}/>
                 </label>
                 <br></br>
                 <div>
                         <h3>Project Cateogary</h3>
-                            <label>
-                                Select an option:
-                                <select name="option" id='cateogary'>
-                                <option value="WEB3">WEB3</option>
-                                <option value="AIML">AIML</option>
-                                <option value="DEV">DEV</option>
-                                <option value="CP">CP</option>
-                                <option value="MISC">MISC</option>
-                                </select>
-                            </label>
+                        <select id='categary' onChange={detailsChange}>
+                            <option value="WEB3">WEB3</option>
+                            <option value="AIML">AIML</option>
+                            <option value="DEV">DEV</option>
+                            <option value="CP">CP</option>
+                            <option value="MISC">MISC</option>
+                        </select>
                         <br></br>
                         <br></br>
                 </div>   
                 <label>
-                    <input type='text' id='description'/>
+                    <input type='text' id='description' onChange={detailsChange}/>
                     Description of the project
                 </label>  
                 <br></br>
                 <label>
-                    <input type='number' id='mentee_min'/>
+                    <input type='number' id='mentee_min' onChange={detailsChange}/>
                     minimum number of mentees
                 </label> 
                 <br></br>
                 <label>
-                    <input type='number' id='mentee_max'/>
+                    <input type='number' id='mentee_max' onChange={detailsChange}/>
                     maximum number of mentees
                 </label> 
                 <br></br>
                 <label>
                     Prereqisite of the mentees(if any)
-                    <input type ='text' id='abstract'/>
+                    <input type ='text' id='abstract' onChange={detailsChange}/>
                 </label>
                 <br></br>
                 <label>
                     timeline(if any)
-                    <input type ='text' id='timeline'/>
+                    <input type ='text' id='timeline' onChange={detailsChange}/>
                 </label>
                 <br></br>
                 <label>
                     upload the banner of the project (as pdf)
-                    <input type='file'id='banner_image'/>
+                    <input type='file'id='banner_image' onChange={detailsChange}/>
                 </label>
             </form>
+            <button onClick={detailsSubmit}>
+                Submit Project
+            </button>
         </>
     )
 }
